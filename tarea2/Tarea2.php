9686e14b5ea0049b77dd8f595940a4c51d385224
RodriiGo <windows_rodrii@hotmail.com>
2013-09-18 12:20:19 -0500

    convercion de dolares a peso

---
+++ tarea2/Tarea2.php
@@ -0,0 +1,12 @@
+<html>
+ <head></head>
+ <body>
+ <?php
+echo "Dolares a Cambiar ".$Dolares=10;
+echo "</br>Tasa de Cambio ".$TasaCambio=15.5;
+$resultado=$Dolares*$TasaCambio;
+echo "</br> En Pesos Mexicanos: ".$resultado;
+ ?>
+ </body>
+ </html>
+
